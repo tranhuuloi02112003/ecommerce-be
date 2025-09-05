f009c47c954c5e06e67147049440a16b1bb308a7
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2025-09-05 20:43:21 +0700

    Init project

---
+++ src/main/java/com/lh/ecormmerce/EcormmerceBeApplication.java
@@ -0,0 +1,13 @@
+package com.lh.ecormmerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcormmerceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcormmerceBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lh/ecormmerce/EcormmerceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lh.ecormmerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcormmerceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
