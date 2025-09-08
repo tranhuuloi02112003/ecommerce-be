ad1f19dcb919ea079e8cd7f77996d9c8af2e48a1
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2025-09-08 20:45:16 +0700

    fix/user-controller

--- src/main/java/com/lh/ecommerce/controller/UserController.java
+++ src/main/java/com/lh/ecommerce/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
   private final UserService userService;
 
-  @GetMapping()
+  @GetMapping
   public List<UserEntity> getAllUser() {
     return userService.getAllUser();
   }
