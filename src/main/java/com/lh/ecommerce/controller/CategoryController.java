2fab47721d8ba4d97e2ecb11d67df274e628b962
tranhuuloi02112003 <tranhuuloi2k3@gmail.com>
2025-09-17 15:00:57 +0700

    feat(api): implement get all categories endpoint

--- src/main/java/com/lh/ecommerce/controller/CategoryController.java
+++ src/main/java/com/lh/ecommerce/controller/CategoryController.java
@@ -3,6 +3,7 @@ package com.lh.ecommerce.controller;
 import com.lh.ecommerce.dto.response.CategoryResponse;
 import com.lh.ecommerce.dto.resquest.CategoryRequest;
 import com.lh.ecommerce.service.category.CategoryService;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -17,5 +18,10 @@ public class CategoryController {
   @ResponseStatus(HttpStatus.CREATED)
   public CategoryResponse create(@RequestBody CategoryRequest request) {
     return categoryService.create(request);
+  }
+
+  @GetMapping
+  public List<CategoryResponse> getAll() {
+    return categoryService.findAll();
   }
 }
--- src/main/java/com/lh/ecommerce/dto/response/CategoryResponse.java
+++ src/main/java/com/lh/ecommerce/dto/response/CategoryResponse.java
@@ -1,13 +1,5 @@
 package com.lh.ecommerce.dto.response;
 
-import java.time.Instant;
 import java.util.UUID;
 
-public record CategoryResponse(
-    UUID id,
-    String name,
-    String description,
-    Instant createdAt,
-    Instant updatedAt,
-    UUID createdBy,
-    UUID updatedBy) {}
+public record CategoryResponse(UUID id, String name, String description) {}
